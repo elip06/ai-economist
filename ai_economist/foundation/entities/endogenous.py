d1223367302776cf8e00ef239b4bfe060324d7f5
elip06 <elitsa.pankovska@gmail.com>
2022-05-24 18:14:10 +0300

    define all endogenous entities of Filecoin system

--- ai_economist/foundation/entities/endogenous.py
+++ ai_economist/foundation/entities/endogenous.py
@@ -34,3 +34,65 @@ class Labor(Endogenous):
     """Labor accumulated through working. Included in all environments by default."""
 
     name = "Labor"
+
+
+@endogenous_registry.add
+class NewData(Endogenous):
+    """The new data or lack thereof that a miner gets to store in the current timestep"""
+
+    name = "NewData"
+
+@endogenous_registry.add
+class TotalData(Endogenous):
+    """The total data capacity a miner is storing"""
+
+    name = "TotalData"
+
+@endogenous_registry.add
+class EnergyPrice(Endogenous):
+    """The electricity costs a miner has to pay per kWh"""
+
+    name = "EnergyPrice"
+
+@endogenous_registry.add
+class RECsPrice(Endogenous):
+    """The cost of the RECs a miner has decided to purchase"""
+
+    name = "RECsPrice"
+
+@endogenous_registry.add
+class GreenScore(Endogenous):
+    """The current green score of a miner, calculated from their scores the last 24h"""
+
+    name = "GreenScore"
+
+@endogenous_registry.add
+class GreenScoresLastDay(Endogenous):
+    """The green scores of a miner from the past 24h"""
+
+    name = "GreenScoresLastDay"
+
+@endogenous_registry.add
+class InitialScore(Endogenous):
+    """The initial green score of a miner, based on the energy mix at their location"""
+
+    name = "InitialGreenScore"
+
+@endogenous_registry.add
+class ReliabilityScore(Endogenous):
+    """The reliability score of a miner"""
+
+    name = "ReliabilityScore"
+
+@endogenous_registry.add
+class TotalScore(Endogenous):
+    """The score of a miner which determines the miner's likelihood of getting to store new data"""
+
+    name = "TotalScore"
+
+@endogenous_registry.add
+class ConsumedEnergy(Endogenous):
+    """The energy amount a miner has consumed in the current timestep"""
+
+    name = "ConsumedEnergy"
+
